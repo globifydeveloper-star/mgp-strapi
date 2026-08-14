dd19f234d9920031d5bd5860657b61615c17e778
globifydeveloper-star <globifydeveloper@gmail.com>
2026-08-14 12:39:19 +0530

    new page creation

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -13,10 +13,130 @@ export interface BlogCta extends Struct.ComponentSchema {
   };
 }
 
+export interface SectionsFaqSection extends Struct.ComponentSchema {
+  collectionName: 'components_sections_faq_sections';
+  info: {
+    description: 'Frequently asked questions section';
+    displayName: 'FAQ Section';
+    icon: 'question';
+  };
+  attributes: {
+    faqs: Schema.Attribute.Relation<'oneToMany', 'api::faq.faq'>;
+    subtitle: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SectionsFeedbackSection extends Struct.ComponentSchema {
+  collectionName: 'components_sections_feedback_sections';
+  info: {
+    description: 'Customer testimonials feedback section';
+    displayName: 'Feedback Section';
+    icon: 'star';
+  };
+  attributes: {
+    subtitle: Schema.Attribute.Text;
+    testimonials: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::testimonial.testimonial'
+    >;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SectionsGoldSellProcessSection extends Struct.ComponentSchema {
+  collectionName: 'components_sections_gold_sell_process_sections';
+  info: {
+    description: 'Gold selling process steps section';
+    displayName: 'Gold Sell Process Section';
+    icon: 'list';
+  };
+  attributes: {
+    process_steps: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::process-step.process-step'
+    >;
+    subtitle: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SectionsGpDifferenceSection extends Struct.ComponentSchema {
+  collectionName: 'components_sections_gp_difference_sections';
+  info: {
+    description: 'The Gold Point Difference section';
+    displayName: 'The Gold Point Difference Section';
+    icon: 'shield';
+  };
+  attributes: {
+    difference_boxes: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::difference-box.difference-box'
+    >;
+    subtitle: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SectionsHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_sections_hero_sections';
+  info: {
+    description: 'Hero section banner with full or half width layout';
+    displayName: 'Hero Section';
+    icon: 'landscape';
+  };
+  attributes: {
+    button1: Schema.Attribute.Component<'blog.cta', false>;
+    button2: Schema.Attribute.Component<'blog.cta', false>;
+    heroImage: Schema.Attribute.Media<'images'>;
+    heroSubtext: Schema.Attribute.Text;
+    heroText: Schema.Attribute.String;
+    layout: Schema.Attribute.Enumeration<['full', 'half']> &
+      Schema.Attribute.DefaultTo<'full'>;
+  };
+}
+
+export interface SectionsOtpEnquirySection extends Struct.ComponentSchema {
+  collectionName: 'components_sections_otp_enquiry_sections';
+  info: {
+    description: 'OTP Enquiry form section';
+    displayName: 'OTP Enquiry Form Section';
+    icon: 'mail';
+  };
+  attributes: {
+    enquiryType: Schema.Attribute.String &
+      Schema.Attribute.DefaultTo<'Enquire Now'>;
+    sourceForm: Schema.Attribute.String &
+      Schema.Attribute.DefaultTo<'Dynamic Page Enquiry Form'>;
+  };
+}
+
+export interface SectionsPromoSliderSection extends Struct.ComponentSchema {
+  collectionName: 'components_sections_promo_slider_sections';
+  info: {
+    description: 'Promo slider section';
+    displayName: 'Promo Slider Section';
+    icon: 'slideshow';
+  };
+  attributes: {
+    promo_slides: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::promo-slide.promo-slide'
+    >;
+  };
+}
+
 declare module '@strapi/strapi' {
   export namespace Public {
     export interface ComponentSchemas {
       'blog.cta': BlogCta;
+      'sections.faq-section': SectionsFaqSection;
+      'sections.feedback-section': SectionsFeedbackSection;
+      'sections.gold-sell-process-section': SectionsGoldSellProcessSection;
+      'sections.gp-difference-section': SectionsGpDifferenceSection;
+      'sections.hero-section': SectionsHeroSection;
+      'sections.otp-enquiry-section': SectionsOtpEnquirySection;
+      'sections.promo-slider-section': SectionsPromoSliderSection;
     }
   }
 }
