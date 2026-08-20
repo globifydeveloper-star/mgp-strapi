3dce803e9146ed65284d52ce2a7fc5363235ead3
globifydeveloper-star <globifydeveloper@gmail.com>
2026-08-20 17:02:44 +0530

    feat: initialize gold-rate-page API with core service, controller, and router

--- src/api/gold-rate-page/controllers/gold-rate-page.ts
+++ src/api/gold-rate-page/controllers/gold-rate-page.ts
@@ -1,2 +1,2 @@
 import { factories } from '@strapi/strapi'
-export default factories.createCoreController('api::gold-rate-page.gold-rate-page');
+export default factories.createCoreController('api::gold-rate-page.gold-rate-page' as any);
