6607f43fa505dbff4606bafb6cd60fb3b7792882
globifydeveloper-star <globifydeveloper@gmail.com>
2026-08-21 18:49:15 +0530

    feat: scaffold mobile-van-page content type and update component schema definitions

---
+++ src/api/mobile-van-page/routes/mobile-van-page.ts
@@ -0,0 +1,7 @@
+/**
+ * mobile-van-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::mobile-van-page.mobile-van-page');
---
+++ src/api/mobile-van-page/services/mobile-van-page.ts
@@ -0,0 +1,7 @@
+/**
+ * mobile-van-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::mobile-van-page.mobile-van-page');
